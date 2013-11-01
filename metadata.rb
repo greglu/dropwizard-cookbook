3249803f2184bdc54211835d0219b4ce388dec47
Greg Lu <greg.lu@gmail.com>
2013-11-01 17:53:04 -0400

    Bumped to version 1.0.1

--- metadata.rb
+++ metadata.rb
@@ -8,7 +8,7 @@ description      'LWRP for dropwizard applications'
 long_description 'Check out the [GitHub docs]' +
                  '(https://github.com/greglu/dropwizard-cookbook) ' +
                  'for more information.'
-version          '1.0.0'
+version          '1.0.1'
 
 depends 'apt'
 depends 'java'
